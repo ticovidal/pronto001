27da35f77f460f190426e8ee92b4b83b996b5884
Thiago <tvidal82@gmail.com>
2018-11-27 10:17:34 -0300

    most

--- app/models/industry.rb
+++ app/models/industry.rb
@@ -1,5 +1,5 @@
 class Industry < ApplicationRecord
   has_many :projects, dependent: :destroy
-  accepts_nested_attributes_for :industries, allow_destroy: true
+  accepts_nested_attributes_for :projects, allow_destroy: true
   belongs_to :profile, optional: true
 end
