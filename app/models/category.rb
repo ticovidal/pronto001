bac1fa7f929a895475ea6b9a66a723ada880fd4a
Thiago <tvidal82@gmail.com>
2018-11-27 07:57:28 -0300

    most

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ApplicationRecord
-  has_many :projects, dependent: :destroy
-  accepts_nested_attributes_for :projects, allow_destroy: true
-  has_many :subcategories, dependent: :destroy
-  accepts_nested_attributes_for :subcategories, allow_destroy: true
+	has_many :subcategories, dependent: :destroy
+	accepts_nested_attributes_for :subcategories, allow_destroy: true
+	has_many :projects, dependent: :destroy
+	accepts_nested_attributes_for :projects, allow_destroy: true
 end
