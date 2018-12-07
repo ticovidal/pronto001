4c1953669c7da054e6f07f9bfb2de2d7c5a52ae7
Thiago <tvidal82@gmail.com>
2018-12-07 19:18:43 -0300

    correcao projects index

--- app/policies/provider_policy.rb
+++ app/policies/provider_policy.rb
@@ -1,4 +1,16 @@
 class ProviderPolicy < ApplicationPolicy
+  def index?
+  	user.superuser? or user.provider?
+  end
+  def new?
+  	user.superuser? or user.provider?
+  end
+  def show?
+  	user.superuser? or user.provider?
+  end
+  def edit?
+  	user.superuser? or user.provider?
+  end	
   class Scope < Scope
     def resolve
       scope
