019accef2a89a2add4a872dda8fa9d23cc3af0ca
Thiago <tvidal82@gmail.com>
2019-01-09 23:38:31 -0300

    propose

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,8 @@
 class DashboardController < ApplicationController
   def industry
   	@projects = Project.where(industry_id:current_user.profile.profileable_id).count
+  	@projectsall = @projects.count
+  	@projectsend = @projects.where(visibility:true).count
   end
 
   def provider
