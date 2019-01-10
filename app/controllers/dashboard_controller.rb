78035b9c19442565e7349c07fb095f4712866a15
Thiago <tvidal82@gmail.com>
2019-01-10 01:20:14 -0300

    propose

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,9 +2,9 @@ class DashboardController < ApplicationController
   def industry
   	@projects = Project.where(industry_id:current_user.profile.profileable_id)
   	@projectsall = Project.where(industry_id:current_user.profile.profileable_id).count
-  	@projectsend1 = Project.where(visibility:false).count
-  	@projectsend2 = Project.where(visibility:true).count
-  	@projectsend3 = Project.first
+  	@projectsend1 = Project.where(industry_id:current_user.profile.profileable_id).where(visibility:false).count
+  	@projectsend2 = Project.where(industry_id:current_user.profile.profileable_id).where(visibility:true).count
+  	@projectsend3 = Project.where(industry_id:current_user.profile.profileable_id).first
   end
 
   def provider
