44d1f5f47fb79908fc9a4bcd127f867b3c128427
Thiago <tvidal82@gmail.com>
2019-01-14 23:07:40 -0300

    propose

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,8 +5,16 @@ class DashboardController < ApplicationController
   	@projectsend1 = Project.where(industry_id:current_user.profile.profileable_id).where(visibility:false).count
   	@projectsend2 = Project.where(industry_id:current_user.profile.profileable_id).where(visibility:true).count
   	@projectsend3 = Project.where(industry_id:current_user.profile.profileable_id).first
+
+  		
   end
 
   def provider
+  	 @projects = Project.joins(:proposes).where(:proposes => {propose_id:current_user.profile.profileable_id})
+  	 @projects = Project.joins(:proposes).where('proposes.provider_id in (?)', current_user.profile.profileable_id)
+  	@projectsall = Project.where(proposes:current_user.profile.profileable_id).count
+  	@projectsend1 = Project.where(proposes:current_user.profile.profileable_id).where(visibility:false).count
+  	@projectsend2 = Project.where(proposes:current_user.profile.profileable_id).where(visibility:true).count
+  	@projectsend3 = Project.where(proposes:current_user.profile.profileable_id).first
   end
 end
