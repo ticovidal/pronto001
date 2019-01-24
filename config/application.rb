0500e3b2d78fa1cfc8442ea859b9d12047005190
Thiago <tvidal82@gmail.com>
2019-01-24 02:05:28 -0300

    proposes

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Pronto001
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.serve_static_assets = true
     config.middleware.insert_before 0, Rack::Cors do
 	 
 	 allow do
