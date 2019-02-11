ad4cd48f3b5b2a0e8ba14d04648d9a9fbe63a9ff
Thiago <tvidal82@gmail.com>
2019-02-11 01:50:41 -0300

    chat

--- config/routes.rb
+++ config/routes.rb
@@ -27,6 +27,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "home#index"
   get 'home/index'
-  get 'dialogs/speeches'
+  get 'dialogs-speeches' => 'dialogs#speeches'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
